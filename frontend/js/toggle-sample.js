4dddb5451139885fab1a51bd5c537c2710e85cd2
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-04-10 15:38:45 -0700

    minor changes

--- frontend/js/toggle-sample.js
+++ frontend/js/toggle-sample.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     overlay.style.top = '63px';
     overlay.style.left = '0';
     overlay.style.width = '100%';
-    overlay.style.height = '96%';
+    overlay.style.height = '97.75%';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
     overlay.style.zIndex = '998';
     document.body.appendChild(overlay);
