ed14460cb92eb67cecda1d4304fea6dd6ed60d80
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-04-10 22:33:25 -0700

    minor changes

---
+++ frontend/js/toggle-pricing.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePricingCardLink = document.querySelector('.pricing-menu');
+    const pricingCard = document.querySelector('.pricing-popup');
+    const mainContent = document.querySelector('main');
+    const overlay = document.createElement('div');
+    
+    // Initialize overlay styles
+    overlay.className = 'overlay'; // Ensure this class is applied for initial CSS styling
+    overlay.style.position = 'absolute';
+    overlay.style.top = '63px';
+    overlay.style.left = '0';
+    overlay.style.width = '100%';
+    overlay.style.height = '97.75%';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    overlay.style.zIndex = '998';
+    document.body.appendChild(overlay);
+
+    // Toggle visibility of the sample card and overlay
+    togglePricingCardLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        pricingCard.classList.toggle('show');
+        overlay.classList.toggle('show');
+        if (pricingCard.classList.contains('show')) {
+            mainContent.style.position = 'relative';
+            pricingCard.style.zIndex = '999';
+        }
+    });
+
+    // Hide sample card and overlay when overlay is clicked
+    overlay.addEventListener('click', function() {
+        pricingCard.classList.remove('show');
+        overlay.classList.remove('show');
+    });
+});
