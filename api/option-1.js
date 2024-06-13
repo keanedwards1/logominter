090a1500a39dcd8cac2d0609dcf140f5c94c341e
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-06-13 11:27:10 -0700

    minor changes

--- api/option-1.js
+++ api/option-1.js
@@ -1,3 +1,24 @@
+/* 
+Serverless API Example From Hugging Face
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/artificialguybr/LogoRedmond-LogoLoraForSDXL-V2",
+		{
+			headers: { Authorization: "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+query({"inputs": "Astronaut riding a horse"}).then((response) => {
+	// Use image
+}); */
+
+
+
 const express = require("express");
 const fetch = require("node-fetch");
 const fs = require("fs");
