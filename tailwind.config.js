c4f532e0dbe057e86a9cf27f18e5b1f4028a5b2d
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-07-28 23:08:27 -0700

    added tailwind, made some changes in the menus, still more to do

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,30 +5,4 @@ module.exports = {
     "./*.{html,js,css}", // Includes all HTML, JS, and CSS files in the root directory
     "./backend/app/**/*.{js,py}", // Includes JavaScript and Python files in the backend/app directory, if you ever decide to mix Tailwind classes in server-side templates or scripts
   ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#bae6fd",
-
-          secondary: "#e0f2fe",
-
-          accent: "#d8b4fe",
-
-          neutral: "#e9d5ff",
-
-          "base-100": "#fee2e2",
-
-          info: "#bae6fd",
-
-          success: "#4d7c0f",
-
-          warning: "#fb923c",
-
-          error: "#c2410c",
-        },
-      },
-    ],
-  },
-  plugins: [require("daisyui")],
 };
